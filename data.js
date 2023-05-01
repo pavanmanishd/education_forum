0e7a6cec5710bec87278080a22f01ef2df18c11a
Pavan Manish <pavanmanishd@gmail.com>
2023-05-01 22:16:45 +0530

    test data

---
+++ data.js
@@ -0,0 +1,14 @@
+export const answers = [
+    { 
+        "Qid": "4Vh6j9xsDsDkv0jx1reGM", 
+        "Aid": "sdfhbHBhb", 
+        "answer": "Thisisanswer1", 
+        "username": "pavanmanishd" 
+    }, 
+    { 
+        "Qid": "4Vh6j9xsDsDkv0jx1reGM", 
+        "Aid": "fhgkhkjw", 
+        "answer": "answer2", 
+        "username": "pavanmanishd" 
+    }
+]
