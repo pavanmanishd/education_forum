4efd08cb502493736a3e82ad28771fc30f5f58dd
Pavan Manish <pavanmanishd@gmail.com>
2023-05-01 22:17:56 +0530

    Layout

---
+++ components/Meta.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+export default function Meta() {
+    return (
+        <>
+            <Head>
+                <title>Education Forum</title>
+                <meta name="description" content="A website in which students can post their problems and other studnets can solve them." />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+        </>
+    )
+}
