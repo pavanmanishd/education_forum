0cec53088996b71ee350f17c8daa455bb54ce90b
Pavan Manish <pavanmanishd@gmail.com>
2023-05-04 23:59:35 +0530

    Bug Fix : .map method

--- components/PostsList.js
+++ components/PostsList.js
@@ -1,14 +1,23 @@
 import PostItem from "./PostItem";
+import React from "react";
 import styles from "@/styles/PostsList.module.css";
 export default function PostsList({posts}) {
+    // const [postList, setPostList] = React.useState(posts);
+    // const list = postList.map((post) => (<PostItem post={post} />))
+    // let list = []
+    const [list , setList] = React.useState([])
+    React.useEffect(() => {
+        for (let i = 0; i < posts.length; i++) {
+            setList((prev) => [...prev, <PostItem post={posts[i]} />])
+        }
+    }, [])
+
     return (
         <div className={styles.postsContainer}>
             <h1 className={styles.heading}>Posts</h1>
             <hr />
             <div>
-                {posts.map((post) => (
-                    <PostItem post={post} />
-                ))}
+                {list}
             </div>
         </div>
     );
--- config/index.js
+++ config/index.js
@@ -1,4 +1,4 @@
 const dev = process.env.NODE_ENV !== 'production'
 
 // export const server = dev ? 'http://localhost:3000' : 'https://educationforum-production.up.railway.app'
-export const server = dev ? 'http://localhost:3000' : 'https://education-forum-iwre044ae-pavanmanishd.vercel.app/'
+export const server = dev ? 'http://localhost:3000' : 'https://education-forum-pavanmanishd.vercel.app/'
