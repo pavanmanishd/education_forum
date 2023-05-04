6a77c5210079919d0b599fad60c1d932fb912920
Pavan Manish <pavanmanishd@gmail.com>
2023-05-05 00:50:51 +0530

    external api

--- config/index.js
+++ config/index.js
@@ -1,4 +1,6 @@
-const dev = process.env.NODE_ENV !== 'production'
+// const dev = process.env.NODE_ENV !== 'production'
 
 // export const server = dev ? 'http://localhost:3000' : 'https://educationforum-production.up.railway.app'
-export const server = dev ? 'http://localhost:3000' : 'https://education-forum-pavanmanishd.vercel.app/'
+// export const server = dev ? 'http://localhost:3000' : 'https://education-forum-pavanmanishd.vercel.app/'
+
+export const server = 'https://nodejs-production-d50a.up.railway.app'
