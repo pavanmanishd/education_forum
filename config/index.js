3945e6ce7cb7d6ed55395252a0e42bb5a6e167fa
Pavan Manish <pavanmanishd@gmail.com>
2023-05-15 13:35:29 +0530

    api in vercel

--- config/index.js
+++ config/index.js
@@ -3,4 +3,6 @@
 // export const server = dev ? 'http://localhost:3000' : 'https://educationforum-production.up.railway.app'
 // export const server = dev ? 'http://localhost:3000' : 'https://education-forum-pavanmanishd.vercel.app/'
 
-export const server = 'https://nodejs-production-d50a.up.railway.app'
+// export const server = 'https://nodejs-production-d50a.up.railway.app'
+
+export const server = 'https://education-forum.vercel.app' 
